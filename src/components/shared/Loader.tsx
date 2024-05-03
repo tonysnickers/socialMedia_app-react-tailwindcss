206a8f033eed63deba1ec123fd308abe5de9f48f
tonysnickers <antoine.mendy@hotmail.fr>
2024-05-03 19:45:56 +0200

    feature(create_signup_form): creation of signup form

---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,11 @@
+export const Loader = () => (
+  <div className="flex-center w-full">
+    <img
+      src="../../../public/assets/icons/loader.svg"
+      alt="loader"
+      width={24}
+      height={24}
+      className="animate-spin"
+    />
+  </div>
+);
