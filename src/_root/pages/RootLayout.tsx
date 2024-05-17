46687ff74694dda4b0beef299ff125e54ed88a03
tonysnickers <antoine.mendy@hotmail.fr>
2024-05-17 23:24:32 +0200

    feature(creation_leftsidebar_navigation):creation of sidebar navigation

--- src/_root/pages/RootLayout.tsx
+++ src/_root/pages/RootLayout.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Topbar } from "../../components/shared/Topbar";
 import { Outlet } from "react-router-dom";
 import { Bottombar } from "../../components/shared/Bottombar";
+import { LeftSidebar } from "../../components/shared/LeftSidebar";
 
 const RootLayout = () => {
   return (
     <div className="w-full md:flex">
       <Topbar />
+      <LeftSidebar />
       <section className="flex flex-1 h-full">
         <Outlet />
       </section>
