cbe33939dbd7e7e6406ca89aa45a5185078fd4ab
tonysnickers <antoine.mendy@hotmail.fr>
2024-05-19 18:26:39 +0200

    feature(featucreate_footer): creation of footer navigation

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,16 @@ import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/pages/RootLayout";
 import "./globals.css";
 import React from "react";
+import {
+  AllUsers,
+  CreatePost,
+  EditPost,
+  Explore,
+  PostDetails,
+  Profile,
+  Saved,
+  UpdateProfile,
+} from "./_root/pages";
 // import { Toaster } from "./components/ui/toaster";
 
 export const App = () => {
@@ -21,6 +31,14 @@ export const App = () => {
         {/* private routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
       {/* <Toaster /> */}
