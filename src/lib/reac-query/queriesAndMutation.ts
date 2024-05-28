f9e47db40afa9c519a86a148dd9fa6907e68843e
tonysnickers <antoine.mendy@hotmail.fr>
2024-05-28 23:57:30 +0200

    feature(edit_post)wip: creation of edit post

--- src/lib/reac-query/queriesAndMutation.ts
+++ src/lib/reac-query/queriesAndMutation.ts
@@ -5,6 +5,7 @@ import {
   createUserAccount,
   deleteSavedPost,
   getCurrentUser,
+  getPostById,
   getRecentPosts,
   likePost,
   savePost,
@@ -121,5 +122,13 @@ export const useGetCurrentUser = () => {
   return useQuery({
     queryKey: [QUERY_KEYS.GET_CURRENT_USER],
     queryFn: getCurrentUser,
+  });
+};
+
+export const useGetPostById = (postId: string) => {
+  return useQuery({
+    queryKey: [QUERY_KEYS.GET_POST_BY_ID, postId],
+    queryFn: () => getPostById(postId),
+    enabled: !!postId,
   });
 };
