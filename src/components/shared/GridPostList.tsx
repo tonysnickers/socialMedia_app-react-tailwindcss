e1349753dc236bbedcc538cd2bc6e719ee8f9762
tonysnickers <antoine.mendy@hotmail.fr>
2024-06-10 12:20:06 +0200

    feature(display_posts_to_explore_page): displaying post to explore page

--- src/components/shared/GridPostList.tsx
+++ src/components/shared/GridPostList.tsx
@@ -1,7 +1,50 @@
-import React from "react";
+import { useUserContext } from "@/context/AuthContext";
+import { Models } from "appwrite";
+import { Link } from "react-router-dom";
+import PostStats from "./PostStats";
 
-const GridPostList = () => {
-  return <div>GridPostList</div>;
+type GridpostListProps = {
+  posts: Models.Document[];
+  showUser?: boolean;
+  showStats?: boolean;
+};
+
+const GridPostList = ({
+  posts,
+  showUser = true,
+  showStats = true,
+}: GridpostListProps) => {
+  const { user } = useUserContext();
+
+  return (
+    <ul className="grid-container">
+      {posts.map((post) => (
+        <li key={post.$id} className="relative min-w-80 h-80">
+          {post.caption}
+          <Link to={`/posts/${post.$id}`} className="grid-post_link">
+            <img
+              src={post.imageUrl}
+              alt="post"
+              className="h-full w-full object-cover"
+            />
+          </Link>
+          <div className="grid-post_user">
+            {showUser && (
+              <div className="flex items-center justify-start gap-2 flex-1">
+                <img
+                  src={post.creator.imageUrl}
+                  alt="creator"
+                  className="h-8 w-8 rounded-full"
+                />
+                <p className="line-clamp-1">{post.creator.name}</p>
+              </div>
+            )}
+            {showStats && <PostStats post={post} userId={user.id} />}
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default GridPostList;
